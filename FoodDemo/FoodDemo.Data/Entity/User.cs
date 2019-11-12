87590b10e131104f029164751010e55ff5661fbd
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-11-12 16:13:57 +0530

    Detail and partial view

---
+++ FoodDemo/FoodDemo.Data/Entity/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FoodDemo.Data.Entity
+{
+    public class User
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ FoodDemo/FoodDemo.Web/Pages/OrderLines/Detail.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodDemo.Core.ServiceInterface;
+using FoodDemo.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodDemo.Web.Pages.OrderLines
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IOrderLineService orderLineService;
+        private readonly IOrderService orderService;
+
+        public IEnumerable<OrderLine> OrderLines { get; set; }
+        public Order Orders { get; set; }
+        public OrderLine OrderLine { get; set; }
+        public int orderlineid { get; set; }
+
+        public DetailModel(IOrderLineService orderLineService,IOrderService orderService)
+        {
+            this.orderLineService = orderLineService;
+            this.orderService = orderService;
+        }
+
+        public IActionResult OnGet(int orderlineId)
+        {
+            this.orderlineid = orderlineId;
+            OrderLines = orderLineService.GetOrderLine(orderlineId);
+            Orders = orderService.GetOrder(orderlineId);
+
+            if(OrderLines != null)
+            {
+                return Page();
+            }
+            else 
+            {
+                return NotFound();
+            }
+        }
+    }
+}
